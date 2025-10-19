bcad3bd827fab88218f529f18e2cb28dc039c13e
Safo <safohajio@gmail.com>
2025-10-19 16:58:21 +0300

    Initial commit

---
+++ src/test/java/com/example/githupintellijdemo/GithupIntellijDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githupintellijdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithupIntellijDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
