998ea82f80074f6734943617ace079082a6b35f7
Safo <safohajio@gmail.com>
2025-11-07 21:17:54 +0300

    init commiti

---
+++ src/student.java
@@ -0,0 +1,29 @@
+package Student;
+
+public class Student {
+
+    private String name;
+    private int id;
+
+    // Constructor
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', id=" + id + "}";
+    }
+}
+
